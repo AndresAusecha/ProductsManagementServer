fc60c6b764ea9ae795f56d17e62108873cb62696
andres.ausecha <andres@aamsis.co>
2021-05-11 22:50:55 -0500

    add connection to mongo

--- bin/www.js
+++ bin/www.js
@@ -4,18 +4,18 @@ const app = require('../server');
 
 var http = require('http');
 
-const db = require('mongodb').MongoClient;
+const client = require('mongodb').MongoClient;
 
-const dbUrl = "mongodb://localhost:27017/Products";
+const dbUrl = "mongodb://localhost:27017";
 
 app.set('port', port);
 
-db.connect(dbUrl, (err, db) => {
+client.connect(dbUrl, (err, db) => {
   if (err) {
     console.log("Error", err);
     return;
   }
-  app.set('db', db);
+  app.set('db', db.db('Products'));
   const server = http.createServer(app);
   server.listen(port);  
 })
--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (_, res, next) => {
-  res.send({
-    name: 'Some shit',
+router.get('/', (req, res, next) => {
+  const db = req.app.get('db');
+  db.collection('Products').find().toArray((err, documents) => {
+    if(err) {
+      res.send(500).send(err.toString());
+      return;
+    }
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send({
+      products: documents,
+    });
   });
 });
 
